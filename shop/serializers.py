6be6d4f798f769d51f15274e7d555df2c5db719c
iliescountact <countact00@gmail.com>
2022-05-12 10:16:09 +0200

    First serializer nest

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,21 +1,29 @@
 from rest_framework.serializers import ModelSerializer
 
-from shop.models import Category
-from shop.models import Product
-from shop.models import Article
+from shop.models import Category, Product, Article
 
-class CategorySerializer(ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ["id", "name", "date_created", "date_updated", "active"]
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = ["id", "name", "category", "date_created", "date_updated"]
+        fields = ["id", "name"]
+
+
+class CategorySerializer(ModelSerializer):
+
+    # Nous redéfinissons l'attribut 'product' qui porte le même nom que dans la liste des champs à afficher
+    # en lui précisant un serializer paramétré à 'many=True' car les produits sont multiples pour une catégorie
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'date_created', 'date_updated', 'name', 'active', 'products']
 
 class ArticleSerializer(ModelSerializer):
+    # Nous redéfinissons l'attribut 'product' qui porte le même nom que dans la liste des champs à afficher
+    # en lui précisant un serializer paramétré à 'many=True' car les produits sont multiples pour une catégorie
+    # products_art = ProductSerializer(many=True)
     class Meta:
         model = Article
-        fields = ["id", "name", "product_id", "active"]
-        read_only_fields = ['price']
+        fields = ["id", "name", "active", "product"]
+        # read_only_fields = ['price']
